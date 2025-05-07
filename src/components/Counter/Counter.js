adcaec150643784933f7c2e440a78901e784f430
PetrTupec <petrtupec396@seznam.cz>
2025-05-08 00:27:32 +0200

    Enhance counter component with tooltips for increment and decrement buttons, change icon to "+" and "-"

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,25 +1,31 @@
 import "./Counter.css"
 
-const counter = ({ min = 0, max = 999, value, onChange }) => {
+const counter = ({ min = 0, max = 999, value, onChange, toolTipDecrement, tooltipIncrement, toolTipValue }) => {
     const handleDecrement = () => onChange(value - 1)
     const handleIncrement = () => onChange(value + 1)
 
     return (
         <div className="btn-group bg-warning">
             <button
+                title={toolTipDecrement}
                 className="btn btn-warning"
                 onClick={handleDecrement}
                 disabled={value <= min}
             >
-                <i className="fas fa-arrow-left"></i>
+                <i className="fas fa-minus"></i>
             </button>
-            <span className="value">{value}</span>
+            <span
+                title={toolTipValue}
+                className="value">
+                {value}
+            </span>
             <button
+                title={tooltipIncrement}
                 className="btn btn-warning"
                 onClick={handleIncrement}
                 disabled={value >= max}
             >
-                <i className="fas fa-arrow-right"></i>
+                <i className="fas fa-plus"></i>
             </button>
         </div>
     )
