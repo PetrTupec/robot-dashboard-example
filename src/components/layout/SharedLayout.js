4f056974281b829ffebbc0535b49cf7652677ebf
PetrTupec <petrtupec396@seznam.cz>
2025-05-08 03:22:57 +0200

    Refactor SharedLayout; move useTheme from Navbar to ShareDlayouts.

--- src/components/layout/SharedLayout.js
+++ src/components/layout/SharedLayout.js
@@ -1,21 +1,26 @@
 import { useState } from "react"
 import { Outlet } from "react-router-dom"
+import { useTheme } from "../../context/ThemeContext"
 import Navbar from "./Navbar/Navbar"
 import SideNavigation from "./SideNavigation/SideNavigation"
 import Footer from "./Footer/Footer"
 
 const SharedLayout = () => {
-    const [isSidebarVisible, setIsSidebarVisible] = useState(false);
-
-    const handleClickToggle = () => {
-        setIsSidebarVisible(!isSidebarVisible);
-    }
+    const { theme, viewMode, toggleTheme } = useTheme()
+    const [isSideNavigationVisible, setIsSideNavigationVisible] = useState(false);
+   
+    const handleClickToggleMenuVisibility = () => setIsSideNavigationVisible(prev => !prev);
 
     return (
         <>
-            <Navbar onClickToggle={handleClickToggle} />
+            <Navbar
+                theme={theme}
+                viewMode={viewMode}
+                onToggleTheme={toggleTheme}
+                onToggleMenu={handleClickToggleMenuVisibility}
+            />
             <main className="content-ms-custom">
-                <SideNavigation isVisible={isSidebarVisible} />
+                <SideNavigation isVisible={isSideNavigationVisible} />
                 <div className="d-flex flex-column flex-grow-1">
                     <div className="content p-4">
                         <Outlet />
