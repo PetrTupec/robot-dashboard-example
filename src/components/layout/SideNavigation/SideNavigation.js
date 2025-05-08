476e993998cbfed05ee62588977190c84b4d1402
PetrTupec <petrtupec396@seznam.cz>
2025-05-08 23:59:58 +0200

    Refactor SideNavigation; Replace Link with NavLink for improved routing and update CSS for NavLink activate class

--- src/components/layout/SideNavigation/SideNavigation.js
+++ src/components/layout/SideNavigation/SideNavigation.js
@@ -1,5 +1,5 @@
 import "./SideNavigation.css";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const SideNavigation = ({ isVisible }) => {
     const sidenavContainerClass = `sidenav position-fixed top-0 start-0 h-100 " + ${isVisible ? 'd-flex' : 'd-none'} + " d-md-flex flex-column`
@@ -12,17 +12,15 @@ const SideNavigation = ({ isVisible }) => {
                 className={sidenavContainerClass}
             >
                 <div className="sidenav-menu-header text-secondary p-2">Main</div>
-                <ul className="nav nav-pills flex-column mb-auto p-2">
-                    <li className="nav-item">
-                        <Link
-                            to={"/dashboard"}
-                            className="nav-link text-secondary"
-                        >
-                            <i className="fas fa-th-large text-warning"></i>
-                            <span className="ms-2">Dashboard</span>
-                        </Link>
-                    </li>
-                </ul>
+                <nav className="nav flex-column mb-auto p-2">
+                    <NavLink
+                        to={"/dashboard"}
+                        className="nav-link"
+                    >
+                        <i className="fas fa-th-large text-warning"></i>
+                        <span className="ms-2">Dashboard</span>
+                    </NavLink>
+                </nav>
                 <div className="sidenav-menu-footer bg-dark text-secondary">
                     <div className="small">User:</div>
                     <span>admin</span>
